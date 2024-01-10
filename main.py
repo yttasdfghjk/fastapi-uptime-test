d34cea9b37a5bda607b2470ec22ed52becfbfc05
yttasdfghjk <152686179+yttasdfghjk@users.noreply.github.com>
2024-01-10 08:05:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from apscheduler.schedulers.background import BackgroundScheduler
 from contextlib import asynccontextmanager
 import uvicorn
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import ccxt
 import requests
+from pydantic import BaseModel
 
 from datetime import datetime, timezone
 
@@ -128,11 +129,35 @@ app = FastAPI(lifespan=lifespan)
 async def index():
    return {"message": "Hello World"}
 
-"""
-@app.get("/")
+@app.get("/health")
 async def root():
    return {"message": "Hello World"}
-"""
+    
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+    
+    status: str = "OK"
+
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app, host="0.0.0.0", port=8000")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000")
