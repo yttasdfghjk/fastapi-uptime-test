aeb3edd3d9e851ff39d376d0f6fe817813da975d
yttasdfghjk <152686179+yttasdfghjk@users.noreply.github.com>
2024-01-10 08:09:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -160,4 +160,4 @@ def get_health() -> HealthCheck:
     return HealthCheck(status="OK")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
